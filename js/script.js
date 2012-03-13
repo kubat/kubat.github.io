e6f954e80a59f8f2a029096e96dccf7800a1334a
Rony Kubat <rony@kubat.org>
2012-03-13 14:42:47 -0400

    added thesis abstract, cv

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 $(function(){
    $('.hyphenate').css({"text-align":"justify"})
+   $('#thesisAbstract')
+     .hide()
+     .removeClass('hidden')
+     .click(function(){
+       $(this).slideToggle('slow')
+     })
 
+   $('#thesisAbstract img').hover(
+     function() {
+       $(this).attr('src', 'images/thesisPlan2.png')
+     }, 
+     function() {
+       $(this).attr('src', 'images/thesisPlan1.png')
+     });
+
+   $('.abstractToggler').click(function (){
+     $('#thesisAbstract').slideToggle('slow')
+   })
 });
